99e40916da77252a5a0e49245121e3b0c5fb9267
ekaprokokd555 <akyahumaira_22304373@bm400.sch.id>
2024-12-15 19:49:08 +0700

    Update create_luminati_proxy.py

--- create_luminati_proxy.py
+++ create_luminati_proxy.py
@@ -6,9 +6,8 @@ import time
 aws_region = 'us-east-1'  # Ganti dengan region yang sesuai
 ami_id = 'ami-0e2c8caa4b6378d8c'  # Ganti dengan ID AMI (misalnya Ubuntu 20.04)
 instance_type = 't2.micro'  # Pilih tipe instance yang sesuai
-key_name = 'ORA'  # Ganti dengan nama key pair AWS Anda
-security_group = 'sg-01504071d4ce0bc11'  # Ganti dengan security group yang sesuai
-subnet_id = 'subnet-0a0b40983c77acdc1'  # Ganti dengan ID subnet Anda
+key_name = 'PEH'  # Ganti dengan nama key pair AWS Anda
+security_group = 'sg-0a9cbad48c2b3455f'  # Ganti dengan security group yang sesuai
 instance_name = 'Proxy-Residensial-EC2'
 
 # Konfigurasi Luminati (Bright Data)
